e127a2a234311e5493755ff92e1a042453e35e72
Joseph D <j_seremba@live.com>
2017-08-29 15:10:38 +0300

    Add options

--- src/components/DropdownFilter.ts
+++ src/components/DropdownFilter.ts
@@ -1,4 +1,4 @@
-import { Component, createElement } from "react";
+import { Component, OptionHTMLAttributes, ReactElement, createElement } from "react";
 import * as classNames from "classNames";
 
 import "../ui/DropdownFilter.css";
@@ -7,7 +7,7 @@ interface DropdownFilterProps {
     caption: string;
     value: string;
     getFilterValue?: (query: string) => void;
-    options: Array<{ filterAttributeOption: string }>;
+    options: Array<{ filterOptionAttribute: string, filterOptionValue: string}>;
 }
 
 interface DropdownFilterState {
@@ -27,7 +27,7 @@ export class DropdownFilter extends Component<DropdownFilterProps, DropdownFilte
         return (
             createElement("label", { className: classNames("caption-display") }, this.props.caption + ": ",
                 createElement("select", { className: classNames("dropdown-display") }, this.props.options,
-                    createElement("option", {}, this.props.options)))
+                    this.createOptions))
         );
     }
 
@@ -45,5 +45,21 @@ export class DropdownFilter extends Component<DropdownFilterProps, DropdownFilte
 
     private resetQuery() {
         this.setState({ query: "" });
+    }
+
+    private createOptions(props: DropdownFilterProps): Array<ReactElement<{}>> {
+        const optionElements: Array<ReactElement<{}>> = [];
+        if (props.options.length) {
+            props.options.map((optionObject) => {
+                const { filterOptionAttribute, filterOptionValue } = optionObject;
+                const optionValue: OptionHTMLAttributes<HTMLOptionElement> = {
+                    className: "",
+                    label: filterOptionValue,
+                    value: filterOptionAttribute
+                };
+                optionElements.push(createElement("option", optionValue));
+            });
+        }
+        return optionElements;
     }
 }
