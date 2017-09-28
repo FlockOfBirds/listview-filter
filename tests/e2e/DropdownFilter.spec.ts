3dba3760881356ad89845687db6a0e99dac3c225
McDoyen <mrpoloh@yahoo.co.uk>
2017-09-28 21:30:01 +0300

    Update e2e tests

--- tests/e2e/DropdownFilter.spec.ts
+++ tests/e2e/DropdownFilter.spec.ts
@@ -1,7 +1,9 @@
 import defaultFilter from "./pages/default.page";
 
-const dropdownValue = "Uganda";
-
+const dropdownTestValue = "Uganda";
+const dropdownTestValue1 = "Netherlands";
+const dropdownSelect = "#mxui_widget_ReactCustomWidgetWrapper_0 > select";
+const dropdownSelectText = "European countries";
 describe("DropdownFilter", () => {
     it("when dropdown filter is rendered the list view should filter african countries by default", () => {
         defaultFilter.open();
@@ -9,18 +11,16 @@ describe("DropdownFilter", () => {
         defaultFilter.listViewFirstItem.waitForVisible();
 
         const itemValue = defaultFilter.listViewFirstItem.getHTML();
-        expect(itemValue).toContain(dropdownValue);
+        expect(itemValue).toContain(dropdownTestValue);
     });
 
     it("filters a list view by the option selected", () => {
         defaultFilter.open();
         defaultFilter.dropdownFilter.waitForVisible();
         defaultFilter.dropdownFilter.click();
-        defaultFilter.dropdownFilterOption.waitForVisible();
-        defaultFilter.dropdownFilterOption.click();
-        defaultFilter.listViewFirstItem.waitForVisible();
+        browser.selectByVisibleText(dropdownSelect, dropdownSelectText);
 
         const itemValue = defaultFilter.listViewFirstItem.getHTML();
-        expect(itemValue).toContain(dropdownValue);
+        expect(itemValue).toContain(dropdownTestValue1);
     });
 });
--- tests/e2e/pages/default.page.ts
+++ tests/e2e/pages/default.page.ts
@@ -2,8 +2,6 @@ class DefaultFilter {
 
     public get dropdownFilter() { return browser.element(".mx-name-dropdownFilter1"); }
 
-    public get dropdownFilterOption() { return browser.elements(".option:nth-child(8)"); }
-
     public get listViewList() { return browser.elements(".mx-name-listView0"); }
 
     public get listViewFirstItem() { return browser.elements(".mx-listview-item.mx-name-index-0"); }
